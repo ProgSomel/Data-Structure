d267871fb4e17172d6251ff4cc9fba31b4ae60bf
ProgSomel <somelahmed55@gmail.com>
2025-07-01 20:39:19 +0600

    go code added

--- 06 - Singly Linked List Operations/6_4 Insert at any position/go/insertAtAnyPosition.go
+++ 06 - Singly Linked List Operations/6_4 Insert at any position/go/insertAtAnyPosition.go
@@ -1,7 +1,43 @@
 package main
 
+import "fmt"
+
+type Node struct{
+	val int
+	next *Node
+}
+
+func insertAtAnyPosition(head **Node, idx int, value int){
+	newNode := &Node{val: value}
+	temp := *head;
+	for i:=1; i < idx; i++{
+		temp = temp.next;
+	}
+	newNode.next = temp.next;
+	temp.next = newNode
+}
+
+func printLinkedList(head *Node){
+	temp := head;
+	for {
+		if temp == nil{
+			break;
+		}else{
+			fmt.Println(temp.val);
+			temp = temp.next;
+		}
+	}
+}
 
 func main(){
 
+	head := &Node{val: 10}
+	a := &Node{val: 20}
+	b := &Node{val: 30}
 
+	head.next = a;
+	a.next = b;
+
+	insertAtAnyPosition(&head, 2, 100)
+	printLinkedList(head)
 }
