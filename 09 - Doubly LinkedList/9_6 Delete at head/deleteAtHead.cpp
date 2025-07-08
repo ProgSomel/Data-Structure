9f035fdcc3369f5fae15f35e40a7c08514887096
ProgSomel <somelahmed55@gmail.com>
2025-07-09 02:55:52 +0600

    delete head added

---
+++ 09 - Doubly LinkedList/9_6 Delete at head/deleteAtHead.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+    int val;
+    Node* next;
+    Node* prev;
+    Node(int val) {
+        this->val = val;
+        this->prev = NULL;
+        this->next =NULL;
+    }
+};
+
+void deleteAtHead(Node* &head, Node* &tail){
+    Node* deletedNode = head;
+    head = head->next;
+    delete deletedNode;
+    if(head == NULL){
+        tail = NULL;
+        return;
+    }
+    head->prev = NULL;
+}
+
+void printForward(Node* head){
+    Node* tmp = head;
+    while (tmp != NULL)
+    {
+        /* code */
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+    
+}
+
+void printBackward(Node* tail){
+    Node* tmp = tail;
+    while(tmp!=NULL){
+        cout << tmp->val << " ";
+        tmp = tmp->prev;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* tail = new Node(30);
+
+    head->next = a;
+    a->prev = head;
+    a->next = tail;
+    tail->prev = a;
+
+    printForward(head);
+    deleteAtHead(head, tail);
+    printForward(head);
+
+    return 0;
+}
