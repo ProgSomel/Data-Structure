a74891bba5d38bd80010de55bc029df5b1522c5b
ProgSomel <somelahmed55@gmail.com>
2025-07-09 00:29:01 +0600

    printing doubly linked list added

---
+++ 09 - Doubly LinkedList/9_2 Printing Doubly Linked list/printingDoublyLinkedList.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+    int val;
+    Node* next;
+    Node* prev;
+    Node(int val) {
+        this->val = val;
+        this->prev = NULL;
+        this->next =NULL;
+    }
+};
+
+void printForward(Node* head){
+    Node* tmp = head;
+    while (tmp != NULL)
+    {
+        /* code */
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+    
+}
+
+void printBackward(Node* tail){
+    Node* tmp = tail;
+    while(tmp!=NULL){
+        cout << tmp->val << " ";
+        tmp = tmp->prev;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* tail = new Node(30);
+
+    head->next = a;
+    a->prev = head;
+    a->next = tail;
+    tail->prev = a;
+
+    printForward(head);
+    printBackward(tail);
+
+    return 0;
+}
