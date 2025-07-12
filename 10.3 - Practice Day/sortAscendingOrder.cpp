1e6e63b6deb7f72f59dd0638950c0f0baba6e692
ProgSomel <somelahmed55@gmail.com>
2025-07-12 16:33:23 +0600

    problem 5 added

---
+++ 10.3 - Practice Day/sortAscendingOrder.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    list<int>l;
+    while(1){
+        int val;
+        cin >> val;
+        if(val == -1){
+            break;
+        }
+        l.push_back(val);
+    }
+    l.sort();
+    for(int val : l){
+        cout << val << " ";
+    }
+    return 0;
+}
