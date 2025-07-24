9bc1d1a792cd3f46a6be01c656b7cb27d65437be
ProgSomel <somelahmed55@gmail.com>
2025-07-25 02:33:21 +0600

    14_3 Queue Input and Output

---
+++ 14 - Queue Implementation and STL/14_3 Queue Input and Output/queueInputOutput.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+    int val;
+    Node* next;
+    Node(int val) {
+        this->val = val;
+        this->next =NULL;
+    }
+};
+
+
+class myQueue{
+    public:
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    int sizeOfQueue = 0;
+
+    void push(int val){ //? O(1)
+        sizeOfQueue++;
+        Node* newNode = new Node(val);
+        if(head == NULL){
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        tail->next = newNode;
+        tail = newNode;
+    }
+
+    void pop(){ //? O(1)
+        sizeOfQueue--;
+        Node* deletedNode = head;
+        head = head->next;
+        delete deletedNode;
+        if(head == NULL){
+            tail = NULL;
+        }
+    }
+
+    int front(){ //? O(1)
+        return head->val;
+    }
+
+    int back(){ //? O(1)
+        return tail->val;
+    }
+
+    int size(){
+        return sizeOfQueue;
+    }
+
+    bool empty(){
+        return head == NULL;
+    }
+};
+
+int main()
+{   
+    myQueue q;
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        int val;
+        cin >> val;
+        q.push(val);
+    }
+    
+    while(!q.empty()){
+        cout << q.front() << " ";
+        q.pop();
+    }
+
+    return 0;
+}
