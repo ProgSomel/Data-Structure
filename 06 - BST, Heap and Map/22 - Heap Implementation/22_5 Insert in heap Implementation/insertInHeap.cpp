34efbabeb44c8b6f1515fab0421735a018b63a39
Somel Ahmed <progsomel@Somels-MacBook-Pro.local>
2025-08-05 15:16:31 +0600

    22_5 Insert in heap Implementation

---
+++ 06 - BST, Heap and Map/22 - Heap Implementation/22_5 Insert in heap Implementation/insertInHeap.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for(int i = 0; i < n; i++){
+        cin >> v[i];
+    }
+    int val;
+    cin >> val;
+    v.push_back(val);
+    int cur_idx = v.size()-1;
+    int par_idx = (cur_idx-1)/2;
+    while(cur_idx!=0){
+        if(v[par_idx]<v[cur_idx]) swap(v[par_idx], v[cur_idx]);
+        else break;
+        cur_idx = par_idx;
+    }
+    for(int val:v) cout << val << " ";
+    return 0;
+}
